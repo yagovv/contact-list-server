7ba816d0c779cf189d6ab4bacbfe41bf4ea751ed
Yago <yagofvv@gmail.com>
2020-10-14 16:51:18 +0200

    added simple node app

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3030;
 
 app.use(cors());
+
 app.use(express.static('public'));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
